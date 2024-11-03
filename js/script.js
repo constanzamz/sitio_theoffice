d23b3b6ac4eaa15cd02b8465fb6821a80d538710
constanzamz <constanzamartinez036@gmail.com>
2024-11-03 17:33:03 -0300

    actualizacion contacto

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const elements = document.querySelectorAll('.scroll-appear');
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+            observer.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.1
+});
+elements.forEach(el => observer.observe(el));
